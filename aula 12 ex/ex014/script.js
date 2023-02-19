ef977585d008a6a3f60076f017b2bdd9c9d2ac6a
kawaru2 <joaopedrocb2@gmail.com>
2023-02-19 18:56:21 -0300

    finalizando exercício 14

--- aula 12 ex/ex014/script.js
+++ aula 12 ex/ex014/script.js
@@ -3,14 +3,22 @@ function horario() {
     var min = new Date().getMinutes();
     if (hora < 6) {
         window.document.getElementById('hora').innerHTML = `Agora são ${hora} horas e ${min} minutos<br>Tenha uma boa madrugada! 🌜⭐`;
+        window.document.getElementById('img').src = 'Noite.png';
+        window.document.body.style.background = `#090446`;
     }
     else if (hora < 12) {
         window.document.getElementById('hora').innerHTML = `Agora são ${hora} horas e ${min} minutos<br>Tenha um bom dia!🌞`;
+        window.document.getElementById('img').src = `Manhã2.png`;
+        window.document.body.style.background = `#6494aa`;
     }
-    else if (hora < 18) {
+    else if (hora < 19) {
         window.document.getElementById('hora').innerHTML = `Agora são ${hora} horas e ${min} minutos<br>Tenha uma boa tarde! ⛅`;
+        window.document.getElementById(`img`).src = `Tarde.png`;
+        window.document.body.style.background = `#a41e3d`;
     }
     else {
         window.document.getElementById('hora').innerHTML = `São ${hora} horas e ${min} minutos<br>Tenha uma boa noite! 🌙🌟`;
+        window.document.getElementById(`img`).src = `Noite.png`;
+        window.document.body.style.background = `#22162b`;
     }
 }
