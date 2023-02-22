137c0b379da0c2f14c5930460d781333df982de1
kawaru2 <joaopedrocb2@gmail.com>
2023-02-22 18:05:56 -0300

    resolução do exercício 17

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,15 @@
+function calc() {
+    var num = document.getElementById(`tab`).value;
+    var res = document.getElementById(`res`);
+    if (num.length == 0) {
+        alert(`[ERRO] Dados inválidos, tente novamente!`);
+    } else {
+        res.innerHTML = ``;
+        var n = Number(num);
+        for ( var c = 1; c <= 10; c += 1) {
+        var item = document.createElement(`option`);
+        item.text = `${n} x ${c} = ${n*c}`;
+        res.appendChild(item);
+        }
+    }
+}
